406b9b6a27e771c192da30e893cf17538d2f261f
sepi <deet1022@gmail.com>
2016-07-29 09:54:23 +0900

    Add Commands

---
+++ Lab/Lab/Lib/HelloWorld.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab.Lib.Utils;
+using Lab.Lib;
+
+namespace Lab.Lib
+{
+    [Command]
+    public class HelloWorld : ICommand
+    {
+        public Usage CommandUsage
+        {
+            get
+            {
+                Dictionary<string, string> description = new Dictionary<string, string>()
+                {
+                    ["lab helloworld"] = "Shows \"Hello, world!\""
+                };
+                return new Usage("helloworld", description);
+            }
+        }
+
+        public void ExecuteCommand(string[] args)
+        {
+            Log.WriteLine("Hello, world!");
+        }
+    }
+}
---
+++ Lab/Lab/Lib/Version.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Lab.Lib.Utils;
+
+namespace Lab.Lib
+{
+    [Command]
+    public class Version : ICommand
+    {
+        public Usage CommandUsage
+        {
+            get
+            {
+                Dictionary<string, string> description = new Dictionary<string, string>()
+                {
+                    ["lab version"] = "Shows version of the program."
+                };
+                return new Usage("version", description);
+            }
+        }
+
+        public void ExecuteCommand(string[] args)
+        {
+            var version = Assembly.GetExecutingAssembly().GetName().Version;
+            Log.WriteLine($"Lab Version: ");
+            Log.WriteLine($"\t{version.ToString()}");
+        }
+    }
+}
