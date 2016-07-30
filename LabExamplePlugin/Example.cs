74cc58677725641d6279221365b26c8b0deac11c
sepi <deet1022@gmail.com>
2016-07-31 02:31:22 +0900

    Add Example plugin

---
+++ LabExamplePlugin/Example.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lab.Lib;
+using Lab.Lib.Utils;
+
+namespace LabExamplePlugin
+{
+    [Command]
+    public class Example : ICommand
+    {
+        private Dictionary<string, string> commandDescription = new Dictionary<string, string>()
+        {
+            ["lab example"] = "This command is example command."
+        };
+
+        public Usage CommandUsage => new Usage("example", commandDescription);
+
+        public void ExecuteCommand(string[] args)
+        {
+            Log.WriteLine("You enterd example command.");
+        }
+    }
+}
